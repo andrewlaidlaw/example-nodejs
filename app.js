1501d57daff2d648debfd376c48c734b2c4f1587
Andrew Laidlaw <github.com@gwhite.co.uk>
2020-10-26 21:38:07 +0000

    remove line break

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 8080;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from '+host+'/n');
+  res.end('Hello World from '+host);
 });
 
 server.listen(port, hostname, () => {
