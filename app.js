1c693b088da5cc888731bdbb8ae6927286fc2d4b
andrewlaidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2020-10-26 18:21:37 +0000

    First fix

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const http = require('http');
 
 const os = require('os');
-const hostname = os.hostname();
+const hostname = "127.0.0.1";
+const host = os.hostname();
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World from '+hostname);
+  res.end('Hello World from '+host);
 });
 
 server.listen(port, hostname, () => {
