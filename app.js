5649a99b50ec0101299c5a4642f4a85d7b77be74
Andrew Laidlaw <github.com@gwhite.co.uk>
2020-11-15 17:22:00 +0000

    Add line break

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/hello', (req, res) => {
-  res.send('<html><head><style>.center{height: 200px; position: relative; border: 5px solid darkblue; }.center p {font-size: 2em; margin: 0; text-align: center; font-family: Arial, Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%);}</style></head><body><div class="center"><p>Hello from pod '+host+'</p></div></body></html>')
+  res.send('<html><head><style>.center{height: 200px; position: relative; border: 5px solid darkblue; }.center p {font-size: 2em; margin: 0; text-align: center; font-family: Arial, Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%);}</style></head><body><div class="center"><p>Hello from pod <br />'+host+'</p></div></body></html>')
 })
 
 app.listen(port, hostname, () => {
