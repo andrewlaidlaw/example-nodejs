65525a5687b97b3cbba55659b0267719cc041074
Andrew Laidlaw <36501534+andrewlaidlaw@users.noreply.github.com>
2022-09-07 15:40:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const host = os.hostname();
 const secretinfo = process.env.SECRET;
 
 app.get('/', (req, res) => {
-  res.send('<html><head><style>.center{height: 400px; position: relative; border: 5px solid purple; }.center p {font-size: 2em; margin: 0; text-align: center; font-family: Arial, Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%);}</style></head><body><div class="center"><p>Hello from pod <br />'+host+'<br/>The secret is: <em>'+secretinfo+'</em></p></div></body></html>')
+  res.send('<html><head><style>.center{height: 400px; position: relative; border: 5px solid darkblue; }.center p {font-size: 2em; margin: 0; text-align: center; font-family: Arial, Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; -ms-transform: translate(-50%, -50%); transform: translate(-50%, -50%);}</style></head><body><div class="center"><p>Hello from pod <br />'+host+'<br/>The secret is: <em>'+secretinfo+'</em></p></div></body></html>')
 })
 
 app.get('/hello', (req, res) => {
